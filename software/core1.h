764917811d871e500856af2e152ebfb0d89eb4aa
asdfqwer44919 <GgieveMyEBack@protonmail.com>
2024-04-22 22:57:41 +0800

    IDK

--- software/core1.h
+++ software/core1.h
@@ -46,30 +46,131 @@ void core1_entry(void) {
     gpio_put(PWM_PIN3, 1);
 
     uint64_t t_abs, t0, t;      // timing variables
-    float I_target[4];          // target current for the 4 drivers
+    float I_target[4] = { 0.3f , 0.3f , 0.3f , 0.3f };          // target current for the 4 drivers
     int iAD = 0, iBD = 0, iCD = 0, iDD = 0; // indices for diminuendo
     int iA = 0, iB = 0, iC = 0, iD = 0;     // indices for normal lighting
     int iAC = 0, iBC = 0, iCC = 0, iDC = 0; // indices for crescendo
-/*
+	
+	// define the max curent for each channel
+	const float	current_max[4] = { 0.35f , 0.35f , 0.35f , 0.35f };
+
+	/*
     while(gpio_get(T_SYNC)) {   // spin around until T_SYNC line is grounded
         tight_loop_contents();
     }
+	*/
     t0 = to_us_since_boot(get_absolute_time());     // time reference t = 0
 
     while(true) {
         t_abs = to_us_since_boot(get_absolute_time());
         t = t_abs - t0;
+		// deal with nomal lighting
+//			I_target[0] = ((A_s[iA]/(float)127.0)*current_max[0]);
+//			I_target[1] = ((B_s[iB]/(float)127.0)*current_max[1]);
+//			I_target[2] = ((C_s[iC]/(float)127.0)*current_max[2]);
+//			I_target[3] = ((D_s[iD]/(float)127.0)*current_max[3]);
+			/*
+		if ( A_t[iA+1] != (-1) ) {
+			if ( t > A_t[iA] )
+				iA ++;
+			I_target[0] = ((A_s[iA]/(float)127.0)*current_max[0]);
+		}
+		if ( B_t[iB+1] != (-1) ) {
+			if ( t > B_t[iB] )
+				iB ++;
+			I_target[1] = ((B_s[iB]/(float)127.0)*current_max[1]);
+		}
+		if ( C_t[iC+1] != (-1) ) {
+			if ( t > C_t[iC] )
+				iC ++;
+			I_target[2] = ((C_s[iC]/(float)127.0)*current_max[2]);
+		}
+		if ( D_t[iD+1] != (-1) ) {
+			if ( t > D_t[iD] )
+				iD ++;
+			I_target[3] = ((D_s[iD]/(float)127.0)*current_max[3]);
+		}
+		*/
 
+		/*
+		// deal with dim
+		if ( AD_t[iAD] != (-1) ) {
+			if ( t > AD_t[iAD+1] )
+					iAD += 2;
+			if ( t > AD_t[iAD] ) {
+				I_target[0] = ( ( AD_t[iAD+1] - t )/( AD_t[iAD+1] - AD_t[iAD] )* AD_s[iAD] / 
+								127.0f * current_max[0]  );
+				}
+			}
+		if ( BD_t[iBD] != (-1) ) {
+			if ( t > BD_t[iBD+1] )
+					iBD += 2;
+			if ( t > BD_t[iBD] ) {
+				I_target[1] = ( ( BD_t[iBD+1] - t )/( BD_t[iBD+1] - BD_t[iBD] )* BD_s[iBD] / 
+								127.0f * current_max[1]  );
+				}
+			}
+		if ( CD_t[iCD] != (-1) ) {
+			if ( t > CD_t[iCD+1] )
+					iCD += 2;
+			if ( t > CD_t[iCD] ) {
+				I_target[2] = ( ( CD_t[iCD+1] - t )/( CD_t[iCD+1] - CD_t[iCD] )* CD_s[iCD] / 
+								127.0f * current_max[2]  );
+				}
+			}
+		if ( DD_t[iDD] != (-1) ) {
+			if ( t > DD_t[iDD+1] )
+					iDD += 2;
+			if ( t > DD_t[iDD] ) {
+				I_target[3] = ( ( DD_t[iDD+1] - t )/( DD_t[iDD+1] - DD_t[iDD] )* DD_s[iDD] / 
+								127.0f * current_max[3]  );
+				}
+			}
 
+		// deal with cre
+		if ( AC_t[iAC] != (-1) ) {
+			if ( t > AC_t[iAC+1] )
+					iAC += 2;
+			if ( t > AC_t[iAC] ) {
+				I_target[0] = ( ( t - AC_t[iAC] )/( AC_t[iAC+1] - AC_t[iAC] )* AC_s[iAC] / 
+								127.0f * current_max[0]  );
+				}
+			}
+		if ( BC_t[iBC] != (-1) ) {
+			if ( t > BC_t[iBC+1] )
+					iBC += 2;
+			if ( t > BC_t[iBC] ) {
+				I_target[1] = ( ( t - BC_t[iBC] )/( BC_t[iBC+1] - BC_t[iBC] )* BC_s[iBC] / 
+								127.0f * current_max[1]  );
+				}
+			}
+		if ( CC_t[iCC] != (-1) ) {
+			if ( t > CC_t[iCC+1] )
+					iCC += 2;
+			if ( t > CC_t[iCC] ) {
+				I_target[2] = ( ( t - CC_t[iCC] )/( CC_t[iCC+1] - CC_t[iCC] )* CC_s[iCC] / 
+								127.0f * current_max[2]  );
+				}
+			}
+		if ( DC_t[iDC] != (-1) ) {
+			if ( t > DC_t[iDC+1] )
+					iDC += 2;
+			if ( t > DC_t[iDC] ) {
+				I_target[3] = ( ( t - DC_t[iDC] )/( DC_t[iDC+1] - DC_t[iDC] )* DC_s[iDC] / 
+								127.0f * current_max[3]  );
+				}
+			}
+		*/
         // only update value if mutex is not taken
         if(mutex_try_enter(&intensity_mutex, NULL)) {
             intensities[0] = I_target[0];
             intensities[1] = I_target[1];
             intensities[2] = I_target[2];
             intensities[3] = I_target[3];
+            mutex_exit(&intensity_mutex);
         }
     }
-*/
+/*
     while(true) {
         I_target[1] = 0.0f;
         for(int i = 0; i < 256; i++) {
@@ -106,7 +207,7 @@ void core1_entry(void) {
         }
         sleep_ms(3000);
     }
-
+	*/
     return;
 }
 
